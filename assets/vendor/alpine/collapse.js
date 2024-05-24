8334b5a93e8cc8d34b7790a50c96ee798d39d65f
PubliAlex <alexandre@publipresse.fr>
2024-05-24 10:07:59 +0200

    Library update + form partial update

--- assets/vendor/alpine/collapse.js
+++ assets/vendor/alpine/collapse.js
@@ -1,1 +1,99 @@
-(()=>{function g(n){n.directive("collapse",e),e.inline=(t,{modifiers:i})=>{i.includes("min")&&(t._x_doShow=()=>{},t._x_doHide=()=>{})};function e(t,{modifiers:i}){let r=l(i,"duration",250)/1e3,u=l(i,"min",0),h=!i.includes("min");t._x_isShown||(t.style.height=`${u}px`),!t._x_isShown&&h&&(t.hidden=!0),t._x_isShown||(t.style.overflow="hidden");let c=(d,s)=>{let o=n.setStyles(d,s);return s.height?()=>{}:o},f={transitionProperty:"height",transitionDuration:`${r}s`,transitionTimingFunction:"cubic-bezier(0.4, 0.0, 0.2, 1)"};t._x_transition={in(d=()=>{},s=()=>{}){h&&(t.hidden=!1),h&&(t.style.display=null);let o=t.getBoundingClientRect().height;t.style.height="auto";let a=t.getBoundingClientRect().height;o===a&&(o=u),n.transition(t,n.setStyles,{during:f,start:{height:o+"px"},end:{height:a+"px"}},()=>t._x_isShown=!0,()=>{t.getBoundingClientRect().height==a&&(t.style.overflow=null)})},out(d=()=>{},s=()=>{}){let o=t.getBoundingClientRect().height;n.transition(t,c,{during:f,start:{height:o+"px"},end:{height:u+"px"}},()=>t.style.overflow="hidden",()=>{t._x_isShown=!1,t.style.height==`${u}px`&&h&&(t.style.display="none",t.hidden=!0)})}}}}function l(n,e,t){if(n.indexOf(e)===-1)return t;let i=n[n.indexOf(e)+1];if(!i)return t;if(e==="duration"){let r=i.match(/([0-9]+)ms/);if(r)return r[1]}if(e==="min"){let r=i.match(/([0-9]+)px/);if(r)return r[1]}return i}document.addEventListener("alpine:init",()=>{window.Alpine.plugin(g)});})();
+(() => {
+  // packages/collapse/src/index.js
+  function src_default(Alpine) {
+    Alpine.directive("collapse", collapse);
+    collapse.inline = (el, { modifiers }) => {
+      if (!modifiers.includes("min"))
+        return;
+      el._x_doShow = () => {
+      };
+      el._x_doHide = () => {
+      };
+    };
+    function collapse(el, { modifiers }) {
+      let duration = modifierValue(modifiers, "duration", 250) / 1e3;
+      let floor = modifierValue(modifiers, "min", 0);
+      let fullyHide = !modifiers.includes("min");
+      if (!el._x_isShown)
+        el.style.height = `${floor}px`;
+      if (!el._x_isShown && fullyHide)
+        el.hidden = true;
+      if (!el._x_isShown)
+        el.style.overflow = "hidden";
+      let setFunction = (el2, styles) => {
+        let revertFunction = Alpine.setStyles(el2, styles);
+        return styles.height ? () => {
+        } : revertFunction;
+      };
+      let transitionStyles = {
+        transitionProperty: "height",
+        transitionDuration: `${duration}s`,
+        transitionTimingFunction: "cubic-bezier(0.4, 0.0, 0.2, 1)"
+      };
+      el._x_transition = {
+        in(before = () => {
+        }, after = () => {
+        }) {
+          if (fullyHide)
+            el.hidden = false;
+          if (fullyHide)
+            el.style.display = null;
+          let current = el.getBoundingClientRect().height;
+          el.style.height = "auto";
+          let full = el.getBoundingClientRect().height;
+          if (current === full) {
+            current = floor;
+          }
+          Alpine.transition(el, Alpine.setStyles, {
+            during: transitionStyles,
+            start: { height: current + "px" },
+            end: { height: full + "px" }
+          }, () => el._x_isShown = true, () => {
+            if (Math.abs(el.getBoundingClientRect().height - full) < 1) {
+              el.style.overflow = null;
+            }
+          });
+        },
+        out(before = () => {
+        }, after = () => {
+        }) {
+          let full = el.getBoundingClientRect().height;
+          Alpine.transition(el, setFunction, {
+            during: transitionStyles,
+            start: { height: full + "px" },
+            end: { height: floor + "px" }
+          }, () => el.style.overflow = "hidden", () => {
+            el._x_isShown = false;
+            if (el.style.height == `${floor}px` && fullyHide) {
+              el.style.display = "none";
+              el.hidden = true;
+            }
+          });
+        }
+      };
+    }
+  }
+  function modifierValue(modifiers, key, fallback) {
+    if (modifiers.indexOf(key) === -1)
+      return fallback;
+    const rawValue = modifiers[modifiers.indexOf(key) + 1];
+    if (!rawValue)
+      return fallback;
+    if (key === "duration") {
+      let match = rawValue.match(/([0-9]+)ms/);
+      if (match)
+        return match[1];
+    }
+    if (key === "min") {
+      let match = rawValue.match(/([0-9]+)px/);
+      if (match)
+        return match[1];
+    }
+    return rawValue;
+  }
+
+  // packages/collapse/builds/cdn.js
+  document.addEventListener("alpine:init", () => {
+    window.Alpine.plugin(src_default);
+  });
+})();
